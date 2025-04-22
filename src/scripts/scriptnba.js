6767dd3d6e37964adc73627a6ae36d1dd02ffb88
lauty061 <lautyborges2006@gmail.com>
2025-04-22 01:15:39 -0300

    Estadisticas NBA agregada

--- src/scripts/scriptnba.js
+++ src/scripts/scriptnba.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     let fixtureData = ligaData.fixture || [];
     let tablaData = ligaData.tabla_posiciones || {};
+    let estadisticasData = ligaData.estadisticas_jugadores || [];
+
+    mostrarEstadisticas(estadisticasData);
 
     let conferenciaSelect = document.getElementById("conferencia-select");
     let conferencias = Object.keys(tablaData);
@@ -109,6 +112,50 @@ function mostrarTablaPosiciones(tablaData) {
                 <td>${equipo.pc}</td>
                 <td>${equipo.dg}</td>
             </tr>
+        `;
+    });
+}
+
+function mostrarEstadisticas(estadisticas) {
+    const tabla = document.getElementById("tabla-estadisticas-jugadores");
+    if (!estadisticas || !estadisticas.length) {
+        tabla.innerHTML = "<tr><td colspan='12'>No hay datos disponibles</td></tr>";
+        return;
+    }
+
+    tabla.innerHTML = `
+    <tr>
+        <th>Jugador</th>
+        <th>Logo</th>
+        <th>Equipo</th>
+        <th>PJ</th>
+        <th>PP</th>
+        <th>PTS</th>
+        <th>REB</th>
+        <th>AST</th>
+        <th>ROB</th>
+        <th>FG%</th>
+        <th>FT%</th>
+        <th>3P%</th>
+    </tr>
+    `;
+
+    estadisticas.forEach(j => {
+        tabla.innerHTML += `
+        <tr>
+            <td>${j.jugador}</td>
+            <td><img src="${j.logo}" alt="${j.equipo}" width="30" height="30"></td>
+            <td>${j.equipo}</td>
+            <td>${j.pj}</td>
+            <td>${j.pp}</td>
+            <td>${j.pts}</td>
+            <td>${j.reb}</td>
+            <td>${j.ast}</td>
+            <td>${j.rob}</td>
+            <td>${j["fg%"]}</td>
+            <td>${j["ft%"]}</td>
+            <td>${j["3p%"]}</td>
+        </tr>
         `;
     });
 }
