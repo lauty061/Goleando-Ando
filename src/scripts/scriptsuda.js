61f18c36b8b862de8b94273dea681efb30176e27
lauty061 <lautyborges2006@gmail.com>
2025-04-23 16:30:07 -0300

    JSON Update

--- src/scripts/scriptsuda.js
+++ src/scripts/scriptsuda.js
@@ -145,9 +145,9 @@ function mostrarTablaPosiciones(tablaData, grupoSeleccionado = null) {
 
         equipos.forEach((e, index) => {
             let style = "";
-            if (index === 0 || index === 1) {
+            if (index === 0) {
                 style = ' style="background-color: #c8f7c5;"';
-            } else if (index === 2) {
+            } else if (index === 1) {
                 style = ' style="background-color: #b2ebf2;"';
             }
 
